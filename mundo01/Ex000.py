ef2540ee3c7f69a0e0d507b7aea00ccfd4bc30e1
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-07-10 10:46:57 -0300

    first commit

---
+++ mundo01/Ex000.py
@@ -0,0 +1,8 @@
+import this
+nome = input('Qual o seu nome ? ')
+idade = input('Qual a sua idade ?')
+peso = input('Qual o seu peso ? ')
+print('Nome: ', nome, ' Idade: ', idade, ' Peso: ', peso)
+p = input('Digite Primeiro Número de 0 a 9: ')
+s = input('Digite Segundo Número de 0 a 9: ')
+print(int(p)+int(s))
