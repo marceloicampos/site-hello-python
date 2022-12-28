54c23d6027968706df90890d0eb66d4ff6a7843a
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-12-28 07:57:07 -0300

    aula08

---
+++ mundo01/testes/aula08.py
@@ -0,0 +1,25 @@
+import emoji
+import random
+import math
+from math import cos
+from math import floor, sqrt
+num = int(input("Digite um número: "))
+raiz = math.sqrt(num)
+print("A raiz de {} é igual a {}".format(num, raiz))
+print("A raiz de {} arredondado para baixo é igual a {}".format(num, floor(raiz)))
+print("A raiz de {} arredondado para cima é igual a {}".format(num, math.ceil(raiz)))
+print('marcelo')
+print(cos(num))
+print(floor(num))
+print(sqrt(num))
+num = random.random()
+num2 = random.randint(1, 10)
+print(num*10)
+print(num2)
+print(emoji.emojize('Python is :thumbs_up:'))
+# 16 ler um número real e mostre sua porção inteira, onde 6.127 retorne 6.
+# 17 ler cateto oposto e cateto adjacente de um triângulo retângulo retorne cumprimento da hipotenusa.
+# 18 ler um ângulo qualquer e mostre o valor do seno, cosseno e tangente desse ângulo.
+# 19 sortear quatro alunos, lendo os nomes deles e mostre o nome do escolhido.
+# 20 sortear a ordem de apresentação de trabalhos dos aluno. Ler os quatro nomes e mostre a ordem sorteada.
+# 21 faça um programa em python que abra e reproduza o áudio de um arquivo mp3.
