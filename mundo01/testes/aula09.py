bbd5e198edafda4b82100f2fda52a0d782ea6a0a
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-01-17 08:00:43 -0300

    Update aula09.py

--- mundo01/testes/aula09.py
+++ mundo01/testes/aula09.py
@@ -10,9 +10,10 @@ print(len(frase))
 print(frase.count('o'))
 print(frase.count('o', 0, 13))
 print(frase.find('deo'))
+print(frase.rfind('deo'))
 print(frase.find('android'))
 print('Curso' in frase)
-print(frase.replace('Python', 'Paitaon'))
+print(frase.replace('python', 'Paitaon'))
 print(frase.upper())
 print(frase.lower())
 print(frase.capitalize())
@@ -25,7 +26,7 @@ print(frase2.lstrip())
 frase3 = 'Curso em vídeo python'
 print(frase.split())
 print(frase.split()[1])
-print(frase.split()[2][3])
+print(frase.split()[2][0])
 print('-'.join(frase))
 frase3 = frase3.replace('python', 'android')
 print(frase3)
@@ -34,7 +35,7 @@ Lorem ipsum, dolor. Ut ex animi vero. Quo provident nam vitae hic cum !
 Lorem ipsum, dolor. Ut ex animi vero. Quo provident nam vitae hic cum,
 Lorem ipsum dolor sit. Maiores eum dicta ad corporis in vel , hic!, minus !''')
 # 22 Crie programa que leia o nome completo e mostre: o nome em minúsculas, em maiúsculas, quantas letras tem (sem considerar os espaços) e quantas letras tem o primeiro nome
-# 23 Crie um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados. Ex número: 1834, unidade 4, dezena 3 centena 8 e milhar 1 (tem de forma matemática e pro string)
+# 23 Crie um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados. Ex número: 1834, unidade 4, dezena 3 centena 8 e milhar 1 (tem da forma matemática e para string)
 # 24 Crie um programa um programa que leia um nome de uma cidade e diga se ela começa com nome "SANTO"
 # 25 Crie um programa um programa que leia um nome de uma pessoa e diga se ela tem "SILVA"
 # 26 Crie um programa que leia uma frase qualquer e diga: quantas vezes aparece a letra "a", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez
